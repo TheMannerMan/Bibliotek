a7c39a0b295c05dec81e9dbf0d48d6b3b20dad4f
Henri Lehtonen <146133540+TheMannerMan@users.noreply.github.com>
2024-02-23 11:07:00 +0100

    Did some edits on the comments.

--- Program.cs
+++ Program.cs
@@ -1,49 +1,51 @@
-﻿/*
-BÖRJA HÄR
- Hej Nevena. Nedan kommer tankar och funderingar som jag vill dela med mig till dig.
+﻿//The following is notes to my teacher where you can read my thought on the assignment:
+/*
+The following is notes to my teacher where you can read my thought on the assignment. 
+Below are thoughts and considerations that I want to share with you.
 
- Jag har delat upp programmet i 7 klasser vilket är mitt försök att implementera objektorienterad programmering.
- 1. Program.cs - Den del av programmet som initerar övriga klasser och startar huvudmenyn.
- 2. UI.cs - User interface. Här finns de olika menyerna som "pratar" men användaren. Beroende på användarens val så aktiveras andra delar av programmet.
- 3. Borrower.cs - Innehåller uppgifter och metoder om den specifika lånetagaren. T.ex. namn, personnummer, uppgifter om lånadeböcker osv.
- 4. BorrowerHandling.cs - Hanterar lånetagare i allmänhet. Tex. hur de skapas, hur programmet visar information om dem, samt lista med alla lånetagare
- 5. Book.cs - Innehåller uppgifter och metoder för hantering om den specifika boken. T.ex. titel, status, uppgifter om lånetagare, osv.
- 6. BookHandling - Hanterar böcker i allmänhet. Lägga till böcker, låna och returnera böcker, söka på böcker, lista med samtliga böcker.
- 7. DataRepository - Hanterar hämtning och sparning av data externt. I mitt program så sparas data i json format.
+I have divided the program into 7 classes, which is my attempt to implement object-oriented programming.
 
-Allmänt är jag nöjd med fördelningen av klasserna. Jag tycker att jag lyckats rätt väl med Single-responsibility principle. Dvs att en klass ska ha ett ansvar. 
-Det finns dock utmaningar. Jag tycker balansen mellan UI och andra delar är komplex. T.ex. finns det kommunikation mellan programmet och användaren som
-inte sker i UI-objektet. Jag vet inte hur strikt man ska förhålla sig till att alla kommunikation sker via UI? T.ex. innehåller BookHandling ett antal
-metod som frågar användaren om input.
+1. Program.cs - This part of the program initializes other classes and starts the main menu.
+2. UI.cs - User interface. It contains various menus that "communicate" with the user. Depending on the user's choice, other parts of the program are activated.
+3. Borrower.cs - Contains information and methods about the specific borrower, such as name, social security number, details about borrowed books, etc.
+4. BorrowerHandling.cs - Manages borrowers in general. For example, how they are created, how the program displays information about them, and a list of all borrowers.
+5. Book.cs - Contains information and methods for handling the specific book, such as title, status, details about the borrower, etc.
+6. BookHandling - Manages books in general. Adding books, borrowing and returning books, searching for books, and a list of all books.
+7. DataRepository - Handles fetching and saving data externally. In my program, data is saved in JSON format.
 
-För din kännedom så har jag skapat ett antal färdiga objekt för böcker och lånetagare för att kunna testa programmet. Du finner dem under DataRepository.cs.
+Overall, I am satisfied with the distribution of classes. I believe I have succeeded well with the Single-responsibility principle, meaning each class has a specific responsibility.
+However, there are challenges. I find the balance between UI and other parts complex. For example, there is communication between the program and the user that doesn't happen in the 
+UI object. I'm unsure about how strictly one should adhere to having all communication through UI. For instance, BookHandling contains methods that ask the user for input.
 
-ARV: 
-I mitt program har jag inte implementerat arv. I planeringstadiet och i början av utförandet av programmet hade jag svårt att se tydliga samband
-där arv skulle ha kunnat implementeras. Sjävlklart fanns det enkla sätt men som kändes som att de låg utanför ramarna för uppgifte. T.ex. skulle man
-kunna ha haft basklassen bok eller lånetagare samt härledda klasser som Vuxen, barn, vuxenbok och barnbok.
-När jag hade kommit en bra bit in i projektet kunde jag börja se samband där arv med polymorfism hade kunna vara applicerabart. Främst mellan klasserna
-Borrower och Book. Båda är klasser där deras objekt skapas i flera exemplar. Båda klassernas objekt sparas ned och laddas upp till/från JSON. Båda klasserna
-har en PrintOut metod som använs i programmet för att printa ut information om objekten. Jag gav mig ett ett försök med att skapa en Interface för dessa objekt,
-dock hade redan kommit så långt in i projektet, så att implementera interface som skapade gemensamma kontrakt för de två klasserna skapade massvis med konflikter.
-Känslan var sådan att om jag löste en konflikt så skapade den två nya, så jag gav upp med att implementera detta. Men jag vill ändå lyfta att jag i alla fall har 
-funderat över hur arv kan appliceras i programmet.
+For your information, I have created some pre-made objects for books and borrowers to test the program. You can find them under DataRepository.cs.
 
-UTMANINGAR: Jag har haft en del utmaningar längst med vägen och fått arbeta om delar av projektet några gånger. En större bugg jag hade vara att jag fick
-ett felmeddelande om Self referencing loop när programmet skulle spara/ladda upp från JSON. Det tog lång tid att hitta och åtgärda problemet, är 
-fortfarande inte säker om jag förstod problemet fullt ut. Men jag tror problemet var att jag hade två listor med objekt, Borrower och Books. Varje objekt hade 
-varsin lista av varandras objekt. T.ex. hade en instans av lånetagaren en lista med instanser av böcker den lånat. När programmet körde så laddades först en 
-lista av t.ex. lånetagare, sen en lista med böcker. Problemet uppstår när programmet körs och ska skapa en instans av Borrower som inkluderar en
-av en lista med Books, men instanserna av Book ännu inte är skapade. 
-Andra utmaningar jag hade var att få till rätt Access modifiers för klassernas medlemmar, särskilt deras fields och properties. Ibland när jag trodde det skulle
-t.ex vara en propertie med public get och private set så kunde delar av programmet sluta fungera korrekt. 
+---INHERITANCE ---
+I have not implemented inheritance in my program. During the planning stage and the initial execution of the program, I found it challenging to identify clear relationships where 
+inheritance could have been implemented. Of course, there were simple ways, but they felt outside the scope of the task. For example, having a base class Book or Borrower and 
+derived classes like Adult, Child, AdultBook, and ChildBook.
 
-LÄRDOMAR:
-Den största lärdomen jag tar med mig är att fortsättningvis att börja med en MVP (Minimum Viable Product). Istället för att skapa ett program som uppfyller minimum
-kraven så började jag med en massa extra funktioner och metoder för att t.ex. validera personummer och liknande. När jag sedan, efter mycket möda, börjar bli klart
-med programmet så märker jag att en grundläggande funktion, som är ett krav, att spara och ladda data inte fungerar korrekt. Att då, efter man ha skrivit +1000 rader 
-kod börja felsöka och åtgärda problemet var inte enkelt. Fortsättningvis kommer jag skapa en MVP och sedan applicera extra kod som gör programmet bättre.
-*/
+As I progressed in the project, I could see relationships where inheritance with polymorphism could have been applicable, mainly between the Borrower and Book classes. Both are 
+classes where their objects are created in multiple instances. Both classes' objects are saved and loaded to/from JSON. Both classes have a PrintOut method used in the program 
+to print information about the objects. I attempted to create an interface for these objects, but being deep into the project, implementing an interface that created common 
+contracts for the two classes resulted in many conflicts. It felt like solving one conflict created two new ones, so I gave up on implementing this. Still, I want to highlight 
+that I did consider how inheritance could be applied in the program.
+
+--- CHALLENGES ---
+I faced several challenges along the way and had to rework parts of the project several times. A major bug I encountered was a "Self referencing loop" error when the program 
+tried to save/load from JSON. It took a long time to find and address the issue, and I'm still not entirely sure I fully understood it. But I believe the problem was having two 
+lists of objects, Borrower and Books. Each object had its list of each other's objects. For example, an instance of the borrower had a list of instances of books they borrowed. 
+When the program ran, a list of borrowers would be loaded first, then a list of books. The problem arose when the program ran and tried to create an instance of Borrower that 
+included a list of Books, but the instances of Book were not yet created.
+
+Other challenges included getting the right Access modifiers for the class members, especially their fields and properties. Sometimes, when I thought a property should have a 
+public get and private set, certain parts of the program would stop working correctly.
+
+--- LESSONS LEARNED ---
+The most significant lesson I take away is to start with a Minimum Viable Product (MVP). Instead of creating a program with many extra features and methods, such as validating 
+social security numbers, when I started getting close to completing the program, I realized that a fundamental function, saving and loading data, wasn't working correctly. Trying 
+to troubleshoot and fix the problem after writing over 1000 lines of code was not easy. In the future, I will create an MVP and then add additional code to improve the program.
+
+ */
 
 
 using System;
@@ -53,17 +55,17 @@ using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
 using static System.Reflection.Metadata.BlobBuilder;
 
-// Skapa en instans av DataRepository för att hantera datafilinläsning och -skrivning
+// Create an instance of DataRepository to handle data file reading and writing.
 DataRepository repository = new DataRepository();
 
-// Skapa en instans av BorrowerHandling för att hantera låntagare och deras åtgärder
+// Create an instance of BorrowerHandling to manage borrowers and their operations.
 BorrowerHandling myBorrowerHandling = new BorrowerHandling(repository);
 
-// Skapa en instans av BookHandling för att hantera böcker och deras åtgärder
+// Create an instance of BookHandling to manage books and their operations.
 BookHandling myBookHandling = new BookHandling(myBorrowerHandling, repository);
 
-// Skapa en instans av UI (användargränssnittet och deras åtgärder) och skicka med instanserna för bok- och låntagarehantering
+// Create an instance of UI (user interface and its actions) and pass along the instances for book and borrower management.
 UI ui = new UI(myBookHandling, myBorrowerHandling);
 
-// Starta huvudmenyn i användargränssnittet
+// Start the main menu in the user interface
 ui.MainMenu();
